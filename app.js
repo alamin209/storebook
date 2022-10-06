5147485fe4fac474e4e90ea7c9c09b2f481513c0
alamin 209 <mihazahamed209@gmail.com>
2022-10-06 07:53:21 +0600

    added loger and set up

--- app.js
+++ app.js
@@ -1,8 +1,24 @@
-const express = require("express");
-const dotenv = require("dotenv");
+const express = require("express")
+const dotenv = require("dotenv")
 
+//Database Connection path 
+const connectDB = require('./config/db');
+const morgan = require("morgan");
+
+//template engine
+import { engine } from 'express-handlebars';
+
+//Configuration 
 dotenv.config({ path: "./config/config.env" });
+connectDB()
+
 const app = express();
+
+//logging 
+if(process.env.NODE_ENV === 'development'){
+  app.use(morgan('dev'))
+}
+
 const PORT = process.env.PORT || 5000;
 app.listen(
   PORT,
